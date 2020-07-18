3b22871fbeaf1a8d250c1ffd6213d9c7ab0f10ae
stanpressman <68463041+stanpressman@users.noreply.github.com>
2020-07-18 23:17:03 +0300

    New Models

---
+++ MyShop/MyShop.DataAccess.InMemory/ProductRepository.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using MyShop.Core.Models;
+using System.Security.Cryptography.X509Certificates;
+using System.Globalization;
+
+namespace MyShop.DataAccess.InMemory
+{
+    public class ProductRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Product> products;
+
+        public ProductRepository() { 
+            products = cache["products"] as List<Product>;
+            if (products == null) {
+                products = new List<Product>();
+            }
+        
+        }
+        public void Commit()
+        {
+            cache["products"] = products;
+        }
+
+        public void Insert (Product p)
+        {
+            products.Add(p);
+        }
+
+        public void Update(Product product)
+        {
+            Product ProductToUpdate = products.Find(p => p.ID == product.ID);
+            if (ProductToUpdate != null)
+            {
+                ProductToUpdate = product;
+            }
+            else
+            {
+                throw new Exception("Product not found!");
+            }
+        }
+
+        public Product Find(string ID)
+        {
+            Product product = products.Find(p => p.ID == ID);
+            if (product != null)
+            {
+                return product;
+            }
+            else
+            {
+                throw new Exception("Product not found!");
+            }
+        }
+
+        public IQueryable<Product> Collection()
+        {
+            return products.AsQueryable();
+        }
+
+        public void Delete(string ID)
+        {
+            Product ProductToDelete = products.Find(p => p.ID == ID);
+            if (ProductToDelete != null)
+            {
+                products.Remove(ProductToDelete);
+            }
+            else
+            {
+                throw new Exception("Product not found!");
+            }
+        }
+    }
+}
